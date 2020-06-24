338112847f398b2de9c57d338f7dae4c65c680e6
Nokodoko <cMonty614@gmail.com>
2020-06-23 22:00:33 -0400

    Updating my dots repo, with only relvant files plus a list of all files, including non tracked ones)

---
+++ README.md
@@ -0,0 +1,42 @@
+Here are my relevant config directories, as well as a list of all of the programs in my config directory (that are not being tracked):
+
+
+1. alacritty
+2. chromium
+3. cmus
+4. coc
+5. configstore
+6. cordless
+7. dconf
+8. dunst
+9. dunstrc
+10. glava
+11. gtk-3.0
+12. gvim
+13. hangups
+14. htop
+15. i3
+16. jesseduffield
+17. kitty
+18. lf
+19. menus
+20. mimeapps.list
+21. neofetch
+22. neomutt
+23. nvim
+24. obs-studio
+25. pavucontrol.ini
+26. pfetch
+27. powerlevl9k
+28. pulse
+29. QtProject.conf
+30. qutebrowser
+31. ranger
+32. README.md
+33. rofi
+34. Slack
+35. Unknown Organization
+36. vimb
+37. vis
+38. zathura
+39. zoomus.conf
---
+++ cmus/lib.pl
@@ -0,0 +1,28 @@
+/home/n0ko/music/backTo80s.mkv
+/home/n0ko/music/BesOfSynthwave.mkv
+/home/n0ko/music/btm/theRiver.webm
+/home/n0ko/music/btm/theVoid.mkv
+/home/n0ko/music/bullet/Hit The Floor-0Mly84dRIPU.webm
+/home/n0ko/music/covers/Andy James - In The Wake Of Chaos 2017-_erBuhxbuIs.webm
+/home/n0ko/music/covers/Andy James - Time and Time Again [Cover]-BbQ2q7fMDHM.mkv
+/home/n0ko/music/covers/Appassionata Op. 57 - 3rd Movement - Beethoven [Metal]-oFRGg2kiu2k.webm
+/home/n0ko/music/covers/BWV 1052 - J.S. Bach [for Metal Guitar]-EFNFfaWyH_o.mkv
+/home/n0ko/music/covers/Estas Tonne - The Song of the Golden Dragon-7gphiFVVtUI.mkv
+/home/n0ko/music/covers/Scar Symmetry - Ghost Prototype 1 [Cover]-kyqaI7BUuNc.mkv
+/home/n0ko/music/DarkSynth.webm
+/home/n0ko/music/Dragonfly.webm
+/home/n0ko/music/Drake/5amInToronto.webm
+/home/n0ko/music/Drake/6PM In New York-_06iJrd0IbY.webm
+/home/n0ko/music/Drake/9amInDallas.webm
+/home/n0ko/music/Drake/stayScheminRemix.webm
+/home/n0ko/music/JBudden/uncleJoe.webm
+/home/n0ko/music/soundGarden/blackHoleSun.webm
+/home/n0ko/music/soundGarden/outshined.webm
+/home/n0ko/music/soundGarden/showMeHowToLive.webm
+/home/n0ko/music/soundGarden/theDayITriedtoLive.webm
+/home/n0ko/music/stylesP/araabStyles.webm
+/home/n0ko/music/stylesP/morningMourning.mkv
+/home/n0ko/music/stylesP/ruffRyder.webm
+/home/n0ko/music/stylesP/theEssence.mkv
+/home/n0ko/music/Sythwave.webm
+/home/n0ko/music/timeInABottle.mkv
